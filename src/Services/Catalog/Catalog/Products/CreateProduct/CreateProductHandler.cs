270b8022e3cb5bcba19f111e86dfc95652bad0d5
Augusto Henrique de Farias Menna Barreto Monclaro <monclaro.a@gmail.com>
2024-11-17 13:55:20 -0300

    Adding docker-compose and marten

--- src/Services/Catalog/Catalog/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog/Products/CreateProduct/CreateProductHandler.cs
@@ -6,12 +6,15 @@ namespace Catalog.Products.CreateProduct;
 public record CreateProductCommand(string Name, string Description, string ImageFile, decimal Price, List<string> Categories) : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);   
 
-internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductHandler(IDocumentSession documentSession) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
         var product = new Product();
         product.Create(command);
+        
+        documentSession.Store(product);
+        await documentSession.SaveChangesAsync(cancellationToken);
 
         return new CreateProductResult(product.Id);
     }
--- src/Services/Catalog/Catalog/Program.cs
+++ src/Services/Catalog/Catalog/Program.cs
@@ -1,5 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCarter();
+
+//Marten
+builder.Services.AddMarten(o =>
+{
+    o.Connection(builder.Configuration.GetConnectionString("DefaultConnection")!);
+}).UseLightweightSessions();
+
+//Mediatr
 builder.Services.AddMediatR(c =>
 {
     c.RegisterServicesFromAssembly(typeof(Program).Assembly);
